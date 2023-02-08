6c050c11e5bac0dcba2f9afbe41f77442f4a3891
Yuri <yu.rybakin@big3.ru>
2023-02-08 23:39:08 +0300

    Залил скрипт

---
+++ test_items.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time 
+import math
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_abs1(browser):
+        browser.get(link)
+        time.sleep(10)
+        button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+        assert button is not None, "button not found"
+        time.sleep(10)
+        # закрываем браузер после всех манипуляций
+        browser.quit()
