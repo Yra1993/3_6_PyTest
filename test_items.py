617616843a2431cac359b5bc27b2acddd7f74033
Yuri <yu.rybakin@big3.ru>
2023-02-08 23:55:04 +0300

    Изменил название функции

--- test_items.py
+++ test_items.py
@@ -6,7 +6,7 @@ import math
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_abs1(browser):
+def test_find_add_to_card_btn(browser):
         browser.get(link)
         time.sleep(10)
         button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
